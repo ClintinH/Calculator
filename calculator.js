6e987e5be0e70a17e5ca25c002acc4f3c548d685
Clintin Herholdt <clintin4phone@gmail.com>
2022-11-02 20:03:36 +0200

    fixes

--- calculator.js
+++ calculator.js
@@ -1,19 +1,73 @@
+// if final value is computed then delete should not be working
+// if you choose and operand then choose another the operand does not change.
+
 class Calculator {
-  constructor(_, _) {}
+  constructor(curDisplay, preDisplay) {
+    this.currentDisplay = curDisplay;
+    this.previousDisplay = preDisplay;
+    this.clear();
+  }
 
-  clear() {}
+  clear() {
+    //clear entire input
+    this.currentOperand = "";
+    this.previousOperand = "";
+    this.operation = undefined;
+  }
 
-  delete() {}
+  delete() {
+    // delete last add number
+    this.currentOperand = String(this.currentOperand).slice(0, -1);
+  }
 
-  appendNumber(_) {}
+  appendNumber(number) {
+    //prevent user from adding more than 1 . (dot)
+    if (this.currentOperand.includes(".") && number === ".") return;
+    this.currentOperand = this.currentOperand.toString() + number.toString();
+  }
 
-  chooseOperation(_) {}
+  chooseOperation(operation) {
+    if (this.currentOperand === "") return;
+    if (this.previousOperand !== "") this.compute();
+    this.previousOperand = this.currentOperand;
+    this.operation = operation;
+    this.currentOperand = "";
+  }
 
-  compute() {}
+  compute() {
+    let result;
+    let previous = parseFloat(this.previousOperand);
+    let current = parseFloat(this.currentOperand);
 
-  getDisplayNumber(_) {}
+    switch (this.operation) {
+      case "+":
+        result = previous + current;
+        break;
+      case "-":
+        result = previous - current;
+        break;
+      case "*":
+        result = previous * current;
+        break;
+      case "÷":
+        result = previous / current;
+        break;
+    }
+    this.currentOperand = result;
+    this.previousOperand = "";
+    this.operation = undefined;
+  }
 
-  updateDisplay() {}
+  getDisplayNumber(number) {
+    return number;
+  }
+
+  updateDisplay() {
+    this.currentDisplay.innerHTML = this.getDisplayNumber(this.currentOperand);
+    this.previousDisplay.innerHTML = this.operation
+      ? `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+      : "";
+  }
 }
 
 const numberButtons = document.querySelectorAll("[data-number]");
@@ -28,14 +82,42 @@ const currentOperandTextElement = document.querySelector(
   "[data-current-operand]"
 );
 
-const calculator = new Calculator(_, _);
+const calculator = new Calculator(
+  currentOperandTextElement,
+  previousOperandTextElement
+);
 
 //listen to numbers
-
+numberButtons.forEach((numButton) => {
+  numButton.addEventListener("click", function (e) {
+    calculator.appendNumber(e.target.innerText);
+    calculator.updateDisplay();
+  });
+});
 //listen to operations
-
+operationButtons.forEach((button) => {
+  button.addEventListener("click", function (e) {
+    calculator.chooseOperation(e.target.innerText);
+    calculator.updateDisplay();
+  });
+});
 //listen to equals
+equalsButton.addEventListener("click", function (e) {
+  // operations that will calculate numbers into a total
+  calculator.compute();
+  calculator.updateDisplay();
+});
 
 //listen to clear
+allClearButton.addEventListener("click", function (e) {
+  // clear current input
+  calculator.clear();
+  calculator.updateDisplay();
+});
 
 //listen to delete
+deleteButton.addEventListener("click", function (e) {
+  //delete all data of whole operation
+  calculator.delete();
+  calculator.updateDisplay();
+});
